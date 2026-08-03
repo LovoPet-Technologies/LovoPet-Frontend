9051d6198683ebf23f8849e0ef8cd3290ff53224
Iman Datta <dattaiman56@gmail.com>
2026-08-03 22:06:05 +0530

    feat: implement responsive landing page navbar

---
+++ src/components/shared/navbar/navbarLinks.js
@@ -0,0 +1,29 @@
+const navbarLinks = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "Features",
+    path: "#features",
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "#services",
+  },
+  {
+    id: 4,
+    name: "About Us",
+    path: "#about",
+  },
+  {
+    id: 5,
+    name: "Contact Us",
+    path: "#contact",
+  },
+];
+
+export default navbarLinks;
