a6e362cbc224d086ced415fad4860cf1ae109a93
Iman Datta <dattaiman56@gmail.com>
2026-08-20 12:13:50 +0530

    fix: Update navbar and make it dynamic

--- src/components/shared/navbar/navbarLinks.js
+++ src/components/shared/navbar/navbarLinks.js
@@ -1,34 +1,17 @@
-const navbarLinks = [
-  {
-    id: 1,
-    name: "Home",
-    path: "/",
-  },
-  {
-    id: 2,
-    name: "Services",
-    path: "#services",
-  },
-  {
-    id: 3,
-    name: "Why Us",
-    path: "#why-choose-us",
-  },
-  {
-    id: 4,
-    name: "Process",
-    path: "#how-it-works",
-  },
-  {
-    id: 5,
-    name: "Testimonials",
-    path: "#testimonials",
-  },
-  {
-    id: 6,
-    name: "Team",
-    path: "/team",
-  },
+export const homeNavbarLinks = [
+  { id: 1, name: "Home", path: "/" },
+  { id: 2, name: "Services", path: "#services" },
+  { id: 3, name: "Why Us", path: "#why-choose-us" },
+  { id: 4, name: "Process", path: "#how-it-works" },
+  { id: 5, name: "Testimonials", path: "#testimonials" },
+  { id: 6, name: "Team", path: "/team" },
 ];
 
-export default navbarLinks;
+export const appNavbarLinks = [
+  { id: 1, name: "Home", path: "/" },
+  { id: 2, name: "Pet Shop", path: "/pet-shop" },
+  { id: 3, name: "Pharmacy", path: "/pharmacy" },
+  { id: 4, name: "Doctor Consultation", path: "/doctor-consultation" },
+];
+
+export default homeNavbarLinks;
